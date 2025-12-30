fd87d58966aac73e142f3383a90b4bc4c9b080b2
OuLiuh <s.olegd@mail.ru>
2025-12-30 23:56:03 +0300

    fix

--- backend/config.py
+++ backend/config.py
@@ -12,8 +12,8 @@ if dotenv_path.exists():
 POSTGRES_USER = os.getenv('POSTGRES_USER')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 POSTGRES_DB = os.getenv('POSTGRES_DB')
-POSTGRES_HOST = os.getenv('POSTGRES_DB', "db") # Название контейнера в docker
-POSTGRES_PORT = os.getenv('POSTGRES_DB', "5432") # 5432 по умолчанию
+POSTGRES_HOST = os.getenv('POSTGRES_HOST', "db") # Название контейнера в docker
+POSTGRES_PORT = os.getenv('POSTGRES_PORT', "5432") # 5432 по умолчанию
 
 DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
